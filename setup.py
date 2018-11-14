7f2622dd6902fe303bd8c5c3b51a28fdfdac4cac
Squantonias <38762081+Squantonias@users.noreply.github.com>
2018-11-14 15:17:05 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description='The Desenra Discord bot',
     entry_points={
         "console_scripts": [
-            "desenra = desenra.DSB:main",
+            "desenra = desenra.DSB:DSB",
         ]
 }
 )
