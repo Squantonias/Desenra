fa6cae9faf71ac8483e81f59b6b2bfc272e9b642
Squantonias <38762081+Squantonias@users.noreply.github.com>
2018-11-14 15:12:41 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='Desenra',
+    version='1.0',
+    packages=['desenra'],
+    install_requires=['youtube-dl', 'discord.py', 'ffmpeg'],
+    url='https://github.com/Squantonias/DesenraBot',
+    license='',
+    author='Anthony Stonto',
+    author_email='Squantonias@gmail.com',
+    description='The Desenra Discord bot',
+    entry_points={
+        "console_scripts": [
+            "desenra = desenra.DSB:main",
+        ]
+}
+)
